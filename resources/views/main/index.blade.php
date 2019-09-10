d485ad5249cb14b702347186b7c1e9d7ee456fd5
Richie Black <rb15243@gmail.com>
2019-09-09 17:19:27 -0700

    updated homepage content

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -3,5 +3,30 @@
 @section('title', 'Home of richieblack.me')
 
 @section('content')
-    
+    <div class="row">
+        <h1 class="col-12">
+            About Me
+            <img class='richie-image image-round' src="assets/images/richie.jpg" alt="Picture of Richie Black">
+        </h1>
+
+        <div class="col-s-12">
+            <p>
+                I am a life long learner and currently working as a self taught web developer. My current role is
+                focused on development using PHP/JS/MySQL within the Laravel Framework. My main goal is to solve
+                problems while polishing and updating my current skill set. Outside of development, my main interests
+                are personal development, healthy eating, and a budding interest in music.
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <h2 class="col-12">Purpose Of Site</h2>
+        <p class="col-12">
+            The primary purpose of this site is to serve as a host for any <a href="/projects">projects</a> I complete
+            as well as to be a "playground" for messing around with new technologies or showing my skill set. At the
+            moment, I am working on generating a simple blog platform with a backend interface for an admin to use.
+            This entire website is hosted on my
+            <a target="_blank" href="https://github.com/Verest/richieblack.me">GitHub</a>.
+        </p>
+    </div>
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 const mix = require('laravel-mix');
+const imagemin = require('imagemin-webpack-plugin').default;
+const copyWebpack = require('copy-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +13,24 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+mix.webpackConfig({
+    plugins: [
+        new copyWebpack([{
+            from: 'resources/images',
+            to: 'assets/images',
+        }]),
+        new imagemin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            plugins: [
+                imageminMozjpeg({
+                    quality: 90,
+                })
+            ]
+        })
+    ]
+});
+
 
 mix.js('resources/js/app.js', 'public/assets/js');
 
